1ea912074eafa0381d45bc4e8b155564fe6b433b
Гарифинова Гульнара Фанилевна <211909@mck-ktits.ru>
2021-09-02 10:52:52 +0300

    Добавьте файлы проекта.

---
+++ BankApp/Operations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    class Operations
+    {
+        public static void ShowBalance(Account account)
+        {
+            Console.WriteLine(account.Balance);
+        }
+
+        public static void Transaction(Account accountSeller, Account accountGetter, double summ)
+        {
+            if(accountSeller.Balance < -1000)
+            {
+                Console.WriteLine("Недостаточно средств");
+            }
+            else
+            {
+                accountSeller.Balance -= summ;
+                accountGetter.Balance += summ;
+                Console.WriteLine($"balance sell - {accountSeller.Balance}, balance gett - {accountGetter.Balance}");
+            }
+        }
+
+        public static int Widtraw(Account account, double summ)     //Снятие наличных
+        {
+            if (account.Balance < summ)
+            {
+                Console.WriteLine("Недостаточно средств");
+            }
+            else
+            {
+                account.Balance -= summ;
+                return summ;
+            }
+        }
+
+        public static int Deposit()     //Пополнение
+        {
+            return 0;
+        }
+
+        public static void TakeCredit()
+        {
+            ;
+        }
+    }
+}
