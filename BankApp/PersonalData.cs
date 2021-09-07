05acdc7dc704538f4b4cea3a6e2189eea903894c
Гульнара Гарифинова <gulnaragarifinova@yandex.ru>
2021-09-07 14:56:39 +0300

    add field

--- BankApp/PersonalData.cs
+++ BankApp/PersonalData.cs
@@ -6,6 +6,13 @@ namespace BankApp
 {
     struct PersonalData
     {
+        public string name;
+        public int age;
+        Account myAcc;
 
+        public void Info()
+        {
+            Console.WriteLine($"Name: {name}  Age: {age} Acc: {myAcc}");
+        }
     }
 }
